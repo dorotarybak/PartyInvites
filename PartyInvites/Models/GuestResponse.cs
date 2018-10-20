f1194e4bf0fb3f5b7aea0415ade904576b2298ee
Dorota <Dorota@DOROTA>
2018-10-20 16:08:44 +0200

    Add project files.

---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Proszę podać imię")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Proszę podać e-mail")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Proszę podać prawidłowy adres email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Proszę podać nr telefonu")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Proszę określić czy wezmiesz udział w przyjęciu")]
+        public bool? WillAttend { get; set; }
+
+    }
+}
